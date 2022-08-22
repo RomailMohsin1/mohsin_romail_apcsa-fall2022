b9530fd33326a5be0ac12863b144d103046feb02
RomailMohsin1 <111383367+RomailMohsin1@users.noreply.github.com>
2022-08-22 10:43:59 -0700

    Unit 2

---
+++ Unit-02/src/Trap.java
@@ -0,0 +1,15 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  - 
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		double base3 = base1 + base2;
+		double area = 0.5 * (base3) * height;
+		return area;
+	}
+}
